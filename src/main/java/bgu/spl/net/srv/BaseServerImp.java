c3a898329b63b2218e7d0d1467be7efff458fe6f
nir <nirmid@post.bgu.ac.il>
2022-01-05 10:34:13 +0200

    update server imp

--- src/main/java/bgu/spl/net/srv/BaseServerImp.java
+++ src/main/java/bgu/spl/net/srv/BaseServerImp.java
@@ -2,14 +2,14 @@ package bgu.spl.net.srv;
 
 import java.util.function.Supplier;
 
-public class BaseServerImp extends BaseServer{
+public class BaseServerImp<T> extends BaseServer<T>{
 
     public BaseServerImp(int port, Supplier protocolFactory, Supplier encdecFactory) {
         super(port, protocolFactory, encdecFactory);
     }
 
     @Override
-    protected void execute(BlockingConnectionHandler handler) {
+    protected void execute(BlockingConnectionHandler<T> handler) {
         Thread thread = new Thread(handler);
         thread.start();
 
